95291ef788ab657688490b236137ec67b85ede97
Yining Wang <wangyining203@gmail.com>
2022-05-12 01:53:56 +0800

    add Day03 - reverseByTraversal

--- JianZhi/src/Day03/printReverseLinklist.java
+++ JianZhi/src/Day03/printReverseLinklist.java
@@ -21,14 +21,14 @@ public class printReverseLinklist {
 
 
     //用栈实现反转链表
-    public ListNode reverse(ListNode head){
+    public ListNode reverseByRecursiveCall(ListNode head){
         //Stack<Integer> stack = new Stack<>();
 
         if (head == null || head.next == null){
              return head;
         }
         ListNode temp = head.next;
-        ListNode reversedList = reverse(head.next);
+        ListNode reversedList = reverseByRecursiveCall(head.next);
         
         //结束条件 temp的next为head并且head的next为空（指已经反转完了）
         temp.next = head;
@@ -41,4 +41,36 @@ public class printReverseLinklist {
 
 
     //Todo 利用遍历实现反转
+
+    public ListNode reverseByTraversal(ListNode head){
+        ListNode preNode = null;
+        ListNode nextNode = null;
+
+        /*循环过程：
+        在头结点node遍历的时候此时为1结点
+        next = 1结点.next(2结点)
+        1结点.next=pre(null)
+        pre = 1结点
+        node = 2结点
+        进行下一次循环node=2结点
+        next = 2结点.next(3结点)
+        2结点.next=pre(1结点)=>即完成2->1
+        pre = 2结点
+        node = 3结点
+        进行循环…
+        */
+
+        while (head != null){
+            //翻转过程如上述
+            nextNode = head.next;
+            head.next = preNode;
+            preNode = head;
+            head = nextNode;
+        }
+
+        return preNode;
+    }
+
+
+
 }
